1820b0d1b27d9988592e56c7b8375d068ab16b22
Dave Gordon <admin@7asecond.net>
2015-10-15 00:51:00 +0100

    Rebuild

---
+++ Source/Website/App_Code/UmbracoEventHandlers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Core;
+
+namespace Website.App_Code
+{
+    public class UmbracoEventHandlers : Umbraco.Core.ApplicationEventHandler
+    {
+
+        protected override void ApplicationInitialized(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            base.ApplicationInitialized(umbracoApplication, applicationContext);
+        }
+
+    }
+}
